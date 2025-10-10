4988afba63e7b89d11e19599031e7f5d57e0f346
bsimonWallix <bsimon@wallix.com>
2025-10-10 16:11:37 +0200

    fix lint main.go

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
- "github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 
- "github.com/wallix/terraform-provider-wallix-bastion/bastion"
+	"github.com/wallix/terraform-provider-wallix-bastion/bastion"
 )
 
 func main() {
- plugin.Serve(&plugin.ServeOpts{
-  ProviderFunc: bastion.Provider,
- })
+	plugin.Serve(&plugin.ServeOpts{
+		ProviderFunc: bastion.Provider,
+	})
 }
